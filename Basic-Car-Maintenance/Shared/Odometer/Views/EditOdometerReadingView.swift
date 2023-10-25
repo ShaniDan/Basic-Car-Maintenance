3d08643594f646f658ccac0235f2b89b4feea2ea
Shakhnoza Mirabzalova <smirabzalova88@gmail.com>
2023-10-25 06:04:57 +0200

    `AddVehicleView` improvements (#220)

---
+++ Basic-Car-Maintenance/Shared/Odometer/Views/EditOdometerReadingView.swift
@@ -0,0 +1,18 @@
+//
+//  EditOdometerReadingView .swift
+//  Basic-Car-Maintenance
+//
+//  Created by Shakhnoza Mirabzalova on 10/25/23.
+//
+
+import SwiftUI
+
+struct EditOdometerReadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditOdometerReadingView_()
+}
